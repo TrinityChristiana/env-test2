f4e16dc87b24025d25460a47a401d3f24c2e363c
Trinity Christiana <trinitycterry@gmail.com>
2021-02-13 23:20:35 -0600

    Added login and env for netlify

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,14 +1,13 @@
 // USE WITH FIREBASE AUTH
-// import checkLoginStatus from './helpers/authentication';
+import checkLoginStatus from './helpers/auth';
 
 import '../styles/main.scss';
 
 const init = () => {
   document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>';
-  console.log('YOU ARE UP AND RUNNING!');
-  
+  // console.log('YOU ARE UP AND RUNNING!');
   // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+  checkLoginStatus();
 };
 
 init();
